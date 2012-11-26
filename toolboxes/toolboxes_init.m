cad5bdbf1f6f0d0e0456bc277f978261953b658f
Andrea Biasiucci <andrea.biasiucci@epfl.ch>
2012-11-26 16:00:47 +0100

    updated init script fixed simloop fast to get eeg channels from bci.settings

--- toolboxes/toolboxes_init.m
+++ toolboxes/toolboxes_init.m
@@ -6,4 +6,4 @@ mtpath_include('$SMR_TRAIN_ROOT/toolboxes/gdfmatlab');
 mtpath_include('$SMR_TRAIN_ROOT/toolboxes/gkde');
 mtpath_include('$SMR_TRAIN_ROOT/toolboxes/subdir');
 mtpath_include('$SMR_TRAIN_ROOT/toolboxes/xml4mat');
-mtpath_include('$SMR_TRAIN_ROOT/toolboxes/mt_support');
+mtpath_include('$SMR_TRAIN_ROOT/toolboxes/mtmatlab');
