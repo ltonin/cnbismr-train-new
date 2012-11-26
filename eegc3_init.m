106be50c8def5a4ffea6cb39f5482db1e843e0e8
Andrea Biasiucci <andrea.biasiucci@epfl.ch>
2012-11-26 14:30:28 +0100

    updated all init scripts to look for a new envvar

--- eegc3_init.m
+++ eegc3_init.m
@@ -1,17 +1,16 @@
-% 2010-11-11  Michele Tavella <michele.tavella@epfl.ch>
 % Import all directories but modules
 [file, basename] = mtpath_basename(which('eegc3_init'));
 
-mtpath_include('$EEGC3_ROOT/classification/');
-mtpath_include('$EEGC3_ROOT/configuration/');
-mtpath_include('$EEGC3_ROOT/dataset/');
-mtpath_include('$EEGC3_ROOT/featureextraction/');
-mtpath_include('$EEGC3_ROOT/featureselection/');
-mtpath_include('$EEGC3_ROOT/gui/');
-mtpath_include('$EEGC3_ROOT/inputoutput/');
-mtpath_include('$EEGC3_ROOT/integration/');
-mtpath_include('$EEGC3_ROOT/performance/');
-mtpath_include('$EEGC3_ROOT/preprocessing/');
-mtpath_include('$EEGC3_ROOT/tools/');
-mtpath_include('$EEGC3_ROOT/visualization/');
-mtpath_include('$EEGC3_ROOT/toolboxes/');
+mtpath_include('$SMR_OFFLINE_ROOT/classification/');
+mtpath_include('$SMR_OFFLINE_ROOT/configuration/');
+mtpath_include('$SMR_OFFLINE_ROOT/dataset/');
+mtpath_include('$SMR_OFFLINE_ROOT/featureextraction/');
+mtpath_include('$SMR_OFFLINE_ROOT/featureselection/');
+mtpath_include('$SMR_OFFLINE_ROOT/gui/');
+mtpath_include('$SMR_OFFLINE_ROOT/inputoutput/');
+mtpath_include('$SMR_OFFLINE_ROOT/integration/');
+mtpath_include('$SMR_OFFLINE_ROOT/performance/');
+mtpath_include('$SMR_OFFLINE_ROOT/preprocessing/');
+mtpath_include('$SMR_OFFLINE_ROOT/tools/');
+mtpath_include('$SMR_OFFLINE_ROOT/visualization/');
+mtpath_include('$SMR_OFFLINE_ROOT/toolboxes/');
