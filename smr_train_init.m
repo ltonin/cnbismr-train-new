31b23f8a2b8fa622d1ea0b05456e32cc6a2d30f3
Andrea Biasiucci <andrea.biasiucci@epfl.ch>
2012-11-26 14:39:03 +0100

    renamed eegc3_init

---
+++ smr_train_init.m
@@ -0,0 +1,16 @@
+% Import all directories but modules
+[file, basename] = mtpath_basename(which('smr_train_init'));
+
+mtpath_include('$SMR_TRAIN_ROOT/classification/');
+mtpath_include('$SMR_TRAIN_ROOT/configuration/');
+mtpath_include('$SMR_TRAIN_ROOT/dataset/');
+mtpath_include('$SMR_TRAIN_ROOT/featureextraction/');
+mtpath_include('$SMR_TRAIN_ROOT/featureselection/');
+mtpath_include('$SMR_TRAIN_ROOT/gui/');
+mtpath_include('$SMR_TRAIN_ROOT/inputoutput/');
+mtpath_include('$SMR_TRAIN_ROOT/integration/');
+mtpath_include('$SMR_TRAIN_ROOT/performance/');
+mtpath_include('$SMR_TRAIN_ROOT/preprocessing/');
+mtpath_include('$SMR_TRAIN_ROOT/tools/');
+mtpath_include('$SMR_TRAIN_ROOT/visualization/');
+mtpath_include('$SMR_TRAIN_ROOT/toolboxes/');
