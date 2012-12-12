2cfa94a1acedf3de8281777017217fd2e5c7d35c
Andrea Biasiucci <andrea.biasiucci@epfl.ch>
2012-12-12 16:41:40 +0100

    updated autotrain script, now coping with fast train, normalization issues etc. todo: test this!

--- modules/smr/eegc3_smr_autotrain.m
+++ modules/smr/eegc3_smr_autotrain.m
@@ -88,8 +88,10 @@ Classifiers{1}.filename = '';
 Classifiers{1}.filepath = '';
 Classifiers{1}.modality = 'rhlh';
 %%
-
+/
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% HACK: PRESETS -> Trains a Classifier using these features, only. 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 presets.channels = [7 8 10 11 13 15];
 presets.bands = {...           
                             [] ...
@@ -100,9 +102,12 @@ presets.bands = {...
 
 %% 
 
+%% Using no GUI
+usedlg = 0;
+
 % Extract features and labels (prepare dataset)
 disp('[eegc3_smr_autotrain] Extracting/loading features from provided runs...');
-dataset = eegc3_smr_extract(FilePaths, settings, 0);
+dataset = eegc3_smr_extract(FilePaths, settings, usedlg);
 dataset.settings = settings;
 
 % % Saving raw Log-PSD features
@@ -121,32 +126,50 @@ for i = 1:length(Classifiers)
             Classifiers{i}.modality ' Task GDF events: '...
             num2str(Classifiers{i}.task_right) ' '...
             num2str(Classifiers{i}.task_left) ']']);
-       
+		
+		% FOR THE MOMENT, NO NORMALIZATION IS USED!
+		settings.modules.smr.options.selection.norm = false;
+
+		% Save classifier settings
+        Csettings{class_idx} = settings;
+
         % Prepare dataset for feature selection or dataset cropping
         disp('[eegc3_smr_autotrain] Preparing dataset for feature selection');
-        [udataset ndataset] = eegc3_smr_data4selection(dataset, Classifiers{i});
+        [udataset ndataset] = eegc3_smr_data4selection(dataset, Classifiers{i},...
+			settings.modules.smr.options.selection.norm);
+        udataset.settings = Csettings{class_idx};
+        ndataset.settings = Csettings{class_idx};
+		
+		% If intertrial intervals are included, dataset is bound to
+        % contain NaNs in the beginning of each run. Lets get rid of it
+        for r=1:length(udataset.run)
+            notnanind = [];
+            notnanind = find(~isnan(udataset.run{r}.data(:,1)));
+            udataset.run{r}.data = udataset.run{r}.data(notnanind,:);
+            udataset.run{r}.labels = udataset.run{r}.labels(notnanind);
+            udataset.run{r}.trial = udataset.run{r}.trial(1,notnanind);
+        end
         
-        udataset.settings = settings;
-        
-% 		% Saving cropped unnormalized Log-PSD features
-% 		Name = ['PSD_' Classifiers{i}.modality '_' eegc3_daytime() '.mat'];
-% 		disp(['[eegc3_smr_autotrain] Saving cropped unnormalized features'...
-% 		' from provided runs: ' Name]);
-% 		save(Name,'udataset');
+        if(isfield(ndataset,'run'))
+            for r=1:length(ndataset.run)
+                notnanind = [];
+                notnanind = find(~isnan(ndataset.run{r}.data(:,1)));
+                ndataset.run{r}.data = ndataset.run{r}.data(notnanind,:);
+                ndataset.run{r}.labels = ndataset.run{r}.labels(notnanind);
+                ndataset.run{r}.trial = ndataset.run{r}.trial(1,notnanind);
+            end
+        end
 
-        ndataset.settings = settings;
-        
-% 		% Saving cropped normalized Log-PSD features
-% 		Name = ['PSDnorm_' Classifiers{i}.modality '_' eegc3_daytime() '.mat'];
-% 		disp(['[eegc3_smr_autotrain] Saving cropped normalized features'...
-% 		' from provided runs: ' Name]);
-% 		save(Name,'ndataset');
 
         if(isempty(Classifiers{i}.filepath))
             % Feature selection
             disp(['[eegc3_smr_autotrain] Feature selection for classifier: '...
                 Classifiers{i}.modality]);
-            Csettings{class_idx} = eegc3_smr_selection(ndataset, settings);
+			if(~Csettings{class_idx}.modules.smr.options.selection.norm)
+                Csettings{class_idx} = eegc3_smr_selection(udataset, Csettings{class_idx});
+            else
+                Csettings{class_idx} = eegc3_smr_selection(ndataset, Csettings{class_idx});                
+            end
             
             %% HACK: preset tools
             Csettings{class_idx}.bci.smr.channels = presets.channels;
@@ -155,13 +178,18 @@ for i = 1:length(Classifiers)
             %%
             
             % Reshape dataset for classifier training
+			% Here, always use the unnormalized dataset -> udataset, as we will choose
+            % later whether we should normalize before classification. Here
+            % we just want to crop to selected features only
             disp(['[eegc3_smr_autotrain] Reshaping dataset according to feature'...
                 ' selection for classifier: ' Classifiers{i}.modality]);
             cdataset =...
                 eegc3_smr_data4classification(udataset, Csettings{class_idx});
             
             hasInitClassifier = false;
+
         else
+
             % Load previously trained classifier
             disp(['[eegc3_smr_autotrain] Loading initial classifier: ' Classifiers{i}.filepath]);
             settings_prev = load(Classifiers{i}.filepath);
@@ -170,17 +198,26 @@ for i = 1:length(Classifiers)
             else
                 settings_prev = settings_prev.settings;
             end
-            
-            % Compare settings to identify incompatibility between the
+                        
+	
+			% Enrich the modules.classes with the currently requested
+            % classes
+            settings.modules.smr.taskset.classes = ...
+                [Classifiers{i}.task_right Classifiers{i}.task_left];
+    
+			% Compare settings to identify incompatibility between the
             % requested settings and those used to train the initial
             % classifier
+			
             isCompatible = eegc3_smr_comparesettings(settings,settings_prev);
             
             if(isCompatible)
+
                 % Store the previous settings as current settings for this
                 % classifier (along with the feature selection settings)
                 Csettings{class_idx} = settings_prev;
-            else
+            
+			else
                 disp(['[eegc3_smr_autotrain] You have specified an initial'...
                     ' classifier which is not'...
                     ' compatible with the requested settings.'...
@@ -221,18 +258,12 @@ for i = 1:length(Classifiers)
         ylabel('PDF');
         legend('Class 1','Class2');
         
-        % NORMALIZE????
-        disp(['[eegc3_smr_autotrain] I AM NORMALIZING BEFORE CLASSIFICATION']);
-        cndataset.data = zeros(size(cdataset.data));
-        cndataset.labels = cdataset.labels;
-        cndataset.trial = cdataset.trial;
-        cndataset.Paths = cdataset.Paths;
+        % NORMALIZE
+        % No normalization for the moment
+		disp(['[eegc3_smr_autotrain] I AM NOT NORMALIZING BEFORE CLASSIFICATION']);
+        Csettings{class_idx}.modules.smr.options.classification.norm = false;
+        cndataset = cdataset;
         cndataset.settings = Csettings{class_idx};
-            
-        for s=1:size(cdataset.data,1)
-            cndataset.data(s,:) = eegc3_normalize(squeeze(cdataset.data(s,:)));
-        end
-
         
 % 		% Saving cropped, selected, normalized Log-PSD features
 % 		Name = ['PSDselectNorm_' Classifiers{i}.modality '_' eegc3_daytime() '.mat'];
@@ -253,6 +284,7 @@ for i = 1:length(Classifiers)
 		disp('[eegc3_smr_autotrain] Default classification options will be used');  
             
 		% Train classifier
+		% Only train a CNBI GAUSSIAN classifier
 		disp(['[eegc3_smr_autotrain] Training CNBI Gaussian classifier: ' Classifiers{i}.modality]);
 		gau = eegc3_train_gau(Csettings{class_idx}, cndataset.data, ...
 		cndataset.labels, cndataset.trial, hasInitClassifier);
