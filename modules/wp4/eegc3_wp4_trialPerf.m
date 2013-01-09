5c30542eaa29bb93f89d02f5fa378b4255d9ffb1
Andrea Biasiucci <andrea.biasiucci@epfl.ch>
2013-01-09 11:59:54 +0100

    added script to extract single trial performance and save it to .txt file

---
+++ modules/wp4/eegc3_wp4_trialPerf.m
@@ -0,0 +1,28 @@
+function perf = eegc3_wp4_trialPerf(filexdf)
+
+[data.eeg, data.hdr] = sload(filexdf);
+perf = [];
+slashes = find(filexdf == '/');
+fileid = filexdf(slashes(end)+1:end-4);
+% Open text file, save line with run id and update it with the number of
+% deliveries
+if sum(data.hdr.EVENT.TYP == 770) > 0
+    EVENTTYPE = 770;
+else
+    EVENTTYPE = 771;
+end
+
+% Compute Single Trial performance (#of correct deliveries)
+perf.Ndeliveries = sum(data.hdr.EVENT.TYP == 33549);
+perf.Ntrials = sum(data.hdr.EVENT.TYP == EVENTTYPE);
+printf('BCI Performance in this run\n')
+disp(perf);
+
+fileID = fopen([fileid '_performance.txt'],'w');
+
+% Add New Lines
+fprintf(fileID,[fileid '\n']);
+fprintf(fileID,'NCorr   NTrials\n');
+fprintf(fileID,'%.0f %8.0f\n',perf.Ndeliveries,perf.Ntrials);
+
+fclose(fileID);
