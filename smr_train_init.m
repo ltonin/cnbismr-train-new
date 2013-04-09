721f3afbe8245f171ee32346af71b468d7e37cd5
Andrea Biasiucci <andrea.biasiucci@epfl.ch>
2013-04-09 12:12:06 +0200

    moved ricardo's scripts for classifier and subject setting into autotools updated note on eegc3_smr_simloop_fast updated smr_train_init, now only using addpath

--- smr_train_init.m
+++ smr_train_init.m
@@ -13,9 +13,19 @@ addpath([smr_train_root '/preprocessing/']);
 addpath([smr_train_root '/tools/']);
 addpath([smr_train_root '/visualization/']);
 addpath([smr_train_root '/toolboxes/mtmatlab']);
-addpath([smr_train_root '/toolboxes/']);
+addpath([smr_train_root '/toolboxes/matndf']);
 
 addpath([smr_train_root '/modules/smr/']);
 addpath([smr_train_root '/modules/wp4/']);
 addpath([smr_train_root '/modules/eegc2/']);
 addpath([smr_train_root '/modules/cl/']);
+
+addpath([smr_train_root '/toolboxes/biosig']);
+addpath([smr_train_root '/toolboxes/biosig/t200']);
+addpath([smr_train_root '/toolboxes/cva']);
+addpath([smr_train_root '/toolboxes/dptools']);
+addpath([smr_train_root '/toolboxes/gaussian']);
+addpath([smr_train_root '/toolboxes/gdfmatlab']);
+addpath([smr_train_root '/toolboxes/gkde']);
+addpath([smr_train_root '/toolboxes/subdir']);
+addpath([smr_train_root '/toolboxes/xml4mat']);
