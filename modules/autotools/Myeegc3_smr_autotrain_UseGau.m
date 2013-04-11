8086fddd4405bf967f711ae43f4c8b7ba2c6bb55
mcreatura <mcreatura@goya.epfl.ch>
2013-04-11 11:43:20 +0200

    small changes - mainly clean and orderin

--- modules/autotools/Myeegc3_smr_autotrain_UseGau.m
+++ modules/autotools/Myeegc3_smr_autotrain_UseGau.m
@@ -17,61 +17,6 @@
 % classifier eegc3 MAT files (settings)
 
 function [auc, output] = Myeegc3_smr_autotrain_UseGau(FilePaths,sessionNum)
-% 2012  Andrea Biasiucci <andrea.biasiucci@epfl.ch>
-
-%if(nargin < 4)
-%    usedlg = false;
-%end
-%
-%if(nargin == 0)
-%
-%    disp(['[eegc3_smr_autotrain] Please specify cell array of GDF'...
-%        ' filepaths to be used for training or use GUI.']);
-%    
-%        [FilePaths settings Classifiers usedlg] = eegc3_train_gui;
-%        
-%        presets = [];
-%        % Check if close button has been pressed
-%        if(isempty(FilePaths))
-%            return;
-%        end 
-%end
-%
-%if(nargin == 1)
-%    disp('[eegc3_smr_autotrain] Default settings will be used.');
-%    settings = eegc3_newsettings();
-%    settings = eegc3_smr_newsettings(settings);
-%    % No GUI to be used
-%    settings.modules.smr.options.selection.usegui = false;
-%end
-%
-%if(nargin == 2)
-%    disp(['[eegc3_smr_autotrain] No classifier has been specified.'...
-%        ' Attempt to train for default tasksets rhlh, rhbf, lhbf']);
-%    
-%    Classifiers{1}.Enable = true;
-%    Classifiers{2}.Enable = true;
-%    Classifiers{3}.Enable = true;
-%    
-%    Classifiers{1}.task_right = 770;
-%    Classifiers{2}.task_right = 770;
-%    Classifiers{3}.task_right = 771;
-%    
-%    
-%    Classifiers{1}.task_left = 769;
-%    Classifiers{2}.task_left = 771;
-%    Classifiers{3}.task_left = 769;
-%    
-%    
-%    Classifiers{1}.filename = '';
-%    Classifiers{2}.filename = '';
-%    Classifiers{3}.filename = '';
-%    
-%    Classifiers{1}.filepath = '';
-%    Classifiers{2}.filepath = '';
-%    Classifiers{3}.filepath = '';
-%    
-%end
 
 %% HACK: no gui
 % Creates settings
@@ -98,19 +43,11 @@ presets.bands = {...
            []      [10 12]  []  [10 12]     [] ...
            };
 
-%% 
 
 % Extract features and labels (prepare dataset)
 disp('[eegc3_smr_autotrain] Extracting/loading features from provided runs...');
-%dataset = Myeegc3_smr_extract(FilePaths, settings, 0);
 [dataset, output] = Myeegc3_smr_extract_ArtRej(FilePaths, settings, 0);
 dataset.settings = settings;
-
-% % Saving raw Log-PSD features
-% Name = ['PSDall_' eegc3_daytime() '.mat'];
-% disp(['[eegc3_smr_autotrain] Saving raw features from provided runs: '...
-% Name]);
-% save(Name,'dataset');
 
 class_idx = 0;
 % Train all requested classifiers
@@ -129,20 +66,7 @@ for i = 1:length(Classifiers)
             settings.modules.smr.options.selection.norm);
         
         udataset.settings = settings;
-        
-% 		% Saving cropped unnormalized Log-PSD features
-% 		Name = ['PSD_' Classifiers{i}.modality '_' eegc3_daytime() '.mat'];
-% 		disp(['[eegc3_smr_autotrain] Saving cropped unnormalized features'...
-% 		' from provided runs: ' Name]);
-% 		save(Name,'udataset');
-
         ndataset.settings = settings;
-        
-% 		% Saving cropped normalized Log-PSD features
-% 		Name = ['PSDnorm_' Classifiers{i}.modality '_' eegc3_daytime() '.mat'];
-% 		disp(['[eegc3_smr_autotrain] Saving cropped normalized features'...
-% 		' from provided runs: ' Name]);
-% 		save(Name,'ndataset');
 
         if(isempty(Classifiers{i}.filepath))
             % Feature selection
@@ -212,51 +136,9 @@ for i = 1:length(Classifiers)
         
         cdataset.settings = Csettings{class_idx};
         
-% 		% Saving cropped, selected Log-PSD features
-% 		Name = ['PSDselect_' Classifiers{i}.modality '_' eegc3_daytime() '.mat'];
-% 		disp(['[eegc3_smr_autotrain] Saving selected unnormalized'...
-% 		'features from provided runs: ' Name]);
-% 		save(Name,'cdataset');
-%         
-%         eegc3_figure(10 + i);
-%         eegc3_publish(14,14,3,3);
-%         [pdf1 pdf2] = eegc3_smr_cvaspace(cdataset.data,cdataset.labels,[1 2]);
-%         plot(pdf1.x,pdf1.f,'b',pdf2.x,pdf2.f,'r')
-%         title([Classifiers{i}.modality ' dataset in canonical space -- Unnormalized']);
-%         xlabel('1st canonical dimension');
-%         ylabel('PDF');
-%         legend('Class 1','Class2');
         
         %% HACK: Manually removed normalization on Nov 19th 2012
         cndataset = cdataset;
-        % NORMALIZE????
-        %         disp(['[eegc3_smr_autotrain] I AM NORMALIZING BEFORE CLASSIFICATION']);
-        %         cndataset.data = zeros(size(cdataset.data));
-        %         cndataset.labels = cdataset.labels;
-        %         cndataset.trial = cdataset.trial;
-        %         cndataset.Paths = cdataset.Paths;
-        %         cndataset.settings = Csettings{class_idx};
-        %
-        % %         for s=1:size(cdataset.data,1)
-        %             cndataset.data(s,:) = eegc3_normalize(squeeze(cdataset.data(s,:)));
-        %         end
-        %%
-        
-% 		% Saving cropped, selected, normalized Log-PSD features
-% 		Name = ['PSDselectNorm_' Classifiers{i}.modality '_' eegc3_daytime() '.mat'];
-% 		disp(['[eegc3_smr_autotrain] Saving selected normalized'...
-% 		'features from provided runs: ' Name]);
-% 		save(Name,'cndataset');
-% 
-%         eegc3_figure(20 + i);
-%         eegc3_publish(14,14,3,3);
-%         [npdf1 npdf2] = eegc3_smr_cvaspace(cndataset.data,cndataset.labels,[1 2]);
-%         plot(npdf1.x,npdf1.f,'b',npdf2.x,npdf2.f,'r')
-%         title([Classifiers{i}.modality ' dataset in canonical space -- Normalized']);
-%         xlabel('1st canonical dimension');
-%         ylabel('PDF');
-%         legend('Class 1','Class2');
-%         
         
 		disp('[eegc3_smr_autotrain] Default classification options will be used');  
         
@@ -266,29 +148,10 @@ for i = 1:length(Classifiers)
         %%%%%%%%% USE GAU
 		disp(['[eegc3_smr_autotrain] Training CNBI Gaussian classifier: ' Classifiers{i}.modality]);
 
-%%%%%%% It used to be...
-% %	%	   gau = eegc3_train_gau(Csettings{class_idx}, cndataset.data, ...
-% % %		cndataset.labels, cndataset.trial, hasInitClassifier);
-
 %%%%%%% Vasia:
         [gau, auc] = Myeegc3_train_gau(Csettings{class_idx}, cndataset.data, ...
  		cndataset.labels, cndataset.trial, hasInitClassifier,SubID,sessionNum);
         
-        %%%%%%%%% USE LDA
-%         disp(['[eegc3_smr_autotrain] Training CNBI LDA classifier: ' Classifiers{i}.modality]);
-
-%%%%%%% It used to be...
-% % %      lda = eegc3_train_lda(Csettings{class_idx}, cndataset.data, ...
-% % %		cndataset.labels, cndataset.trial);
-
-%%%%%%% Vasia:
-        % Old version of lda (splitting by runs) 
-%         [lda, auc] = Myeegc3_train_lda(Csettings{class_idx}, cndataset.data, ...
-% 		cndataset.labels, cndataset.trial,SubID,sessionNum);
-        %%% New version of lda
-        % [lda, auc] = Myeegc3_train_lda_New(Csettings{class_idx}, cndataset.data, ...
-		% cndataset.labels, cndataset.trial,SubID,sessionNum);
-
         %%%%%%%%%% Uncomment the appropriate one
 		Csettings{class_idx}.bci.smr.gau = gau;
         %Csettings{class_idx}.bci.smr.lda = lda;
@@ -316,12 +179,6 @@ for i = 1:length(Classifiers)
             analysis = eegc3_downgrade_settings(settings);
             NameAnalysis = [SubjectID{1} '_' Modality '_' 'Session' num2str(sessionNum) '_' Date '_auto.mat'];
             
-            %%%% It used to be...
-            %save(NameAnalysis, 'analysis');
-            
-            %%% Vasia:
-            %save(['/homes/vliakoni/Results_LDA_Offline/' NameAnalysis], 'analysis');
-            %save(['/homes/vliakoni/Results_GAU_Rejection/' NameAnalysis], 'analysis');
             save([getenv('TOLEDO_DATA') '/Results/' NameAnalysis(1:find (NameAnalysis=='_')-1) '/Results_GAU_Rejection_'  NameAnalysis], 'analysis');
             disp(['[eegc3_smr_autotrain] Saved eegc2 classifier ' ...
                 Classifiers{i}.modality ': ' NameAnalysis]);
