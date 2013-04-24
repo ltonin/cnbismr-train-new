2bcba1c9af0b9f4e0d5fc26732cfda5a7e14d4b8
BCI-TDCS Experiment <cnbiclinic03@CNBI-NB-05.(none)>
2013-04-24 16:31:26 +0200

    made init script simple

--- smr_train_init.m
+++ smr_train_init.m
@@ -1,31 +1,4 @@
 smr_train_root = getenv('SMR_TRAIN_ROOT');
+addpath('/usr/share/cnbiloop/cnbitkmat/mtpath/');
 
-addpath([smr_train_root '/classification/']);
-addpath([smr_train_root '/configuration/']);
-addpath([smr_train_root '/dataset/']);
-addpath([smr_train_root '/featureextraction/']);
-addpath([smr_train_root '/featureselection/']);
-addpath([smr_train_root '/gui/']);
-addpath([smr_train_root '/inputoutput/']);
-addpath([smr_train_root '/integration/']);
-addpath([smr_train_root '/performance/']);
-addpath([smr_train_root '/preprocessing/']);
-addpath([smr_train_root '/tools/']);
-addpath([smr_train_root '/visualization/']);
-addpath([smr_train_root '/toolboxes/mtmatlab']);
-addpath([smr_train_root '/toolboxes/matndf']);
-
-addpath([smr_train_root '/modules/smr/']);
-addpath([smr_train_root '/modules/wp4/']);
-addpath([smr_train_root '/modules/eegc2/']);
-addpath([smr_train_root '/modules/cl/']);
-
-addpath([smr_train_root '/toolboxes/biosig']);
-addpath([smr_train_root '/toolboxes/biosig/t200']);
-addpath([smr_train_root '/toolboxes/cva']);
-addpath([smr_train_root '/toolboxes/dptools']);
-addpath([smr_train_root '/toolboxes/gaussian']);
-addpath([smr_train_root '/toolboxes/gdfmatlab']);
-addpath([smr_train_root '/toolboxes/gkde']);
-addpath([smr_train_root '/toolboxes/subdir']);
-addpath([smr_train_root '/toolboxes/xml4mat']);
+addpath(genpath(smr_train_root));
