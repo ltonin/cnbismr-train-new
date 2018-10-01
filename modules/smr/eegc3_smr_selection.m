769caf1556df009609937c8a580270296b7035eb
Luca Tonin <luca.tonin@epfl.ch>
2018-10-01 14:31:38 +0200

    Added feature selection on all runs together

--- modules/smr/eegc3_smr_selection.m
+++ modules/smr/eegc3_smr_selection.m
@@ -45,6 +45,46 @@ if(settings.modules.smr.options.selection.stability)
             selection.Alldpa,selection.Alldpm,settings);
     end
     
+elseif(settings.modules.smr.options.selection.overall)
+    disp('[eegc3_smr_selection] Running feature selection: overall');
+    % Concatenate all runs
+    adataset.data = [];
+    adataset.labels = [];
+    
+    RunNum = length(dataset.run);
+    for r=1:RunNum
+    
+        adataset.data = [adataset.data; dataset.run{r}.data];
+        adataset.labels = [adataset.labels; dataset.run{r}.labels];
+        
+    end
+    
+    % Selection on the overall runs
+    [selection.Alldpa, selection.Allchannels, selection.Allbands, ...
+	selection.Allbandsidx, selection.Alltot, selection.Alldpm,...
+	dummy1, dummy2] = ...
+		eegc3_smr_select(adataset, settings);
+    
+    % Selection stability
+    selection_stability = eegc3_smr_select_stable(dataset, settings);
+
+    % Put into settings structure the result of selection
+    settings.bci.smr.channels = selection.Allchannels;
+    settings.bci.smr.bands = selection.Allbands; 
+    
+    % Plots 
+    eegc3_dpplot(101, selection.Alldpa, [0 1], ...
+        1:settings.acq.channels_eeg, settings.modules.smr.psd.freqs, 1);
+    
+    eegc3_dpplot(102, selection.Alldpm, [0 1], ...
+        1:settings.acq.channels_eeg, settings.modules.smr.psd.freqs, 1);
+    
+	% Feature selection GUI evoked here
+    if(settings.modules.smr.options.selection.usegui)
+        [settings Pbnidx Ptot] = ...
+            eegc3_select_gui(selection_stability.Pdpa,...
+            selection.Alldpa,selection.Alldpm,settings);
+    end
 else
     
     disp('[eegc3_smr_selection] Running feature selection: batch');
@@ -75,5 +115,5 @@ else
     
     eegc3_dpplot(102, selection.Alldpm, [0 1], ...
         1:settings.acq.channels_eeg, settings.modules.smr.psd.freqs, 1);
-    
+   
 end
